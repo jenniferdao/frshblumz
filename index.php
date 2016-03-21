a4a706a7ea12aea1eb8953e590ade77549ec6800
zubinkhan <zubin.khan@mail.utoronto.ca>
2016-03-20 20:00:31 -0400

    Index Sheet was modified

--- index.php
+++ index.php
@@ -17,40 +17,54 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) :
+	<div id="gridcontainer">
+<?php 
+	$counter = 1; //start counter
+	$grids = 2; //Grids per row
+	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+	$args = array('posts_per_page' => 5, 'paged' => $paged, 'cat'=>30 );
+	
+	query_posts( 'category_name=Markup&posts_per_page=5&paged=' . $paged ); 
+	if(have_posts()) :  while(have_posts()) :  the_post();
+?>
+<?php
+//Show the left hand side column
+if($counter == 1) :
+?>
+	<div class="griditemleft">
+	<div class="postimage">
+<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('category-thumbnail'); ?></a>
+	</div>
+	<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	</div>
+<?php
+	//Show the right hand side column
+	elseif($counter == $grids) :
+?>
+	<div class="griditemright">
+		<div class="postimage">
+			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_post_thumbnail('category-thumbnail'); ?></a>
+		</div>
+			<h2><a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+		</div>
+	<div class="clear"></div>
+<?php
+$counter = 0;
+endif;
+?>
+<?php
+$counter++;
+endwhile; ?>
+<?php the_posts_pagination( $args ); ?>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
 
-			<?php
-			endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
+<?php endif;
+?>
+</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
-
 <?php
 get_sidebar();
 get_footer();
