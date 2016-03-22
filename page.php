2095ca7e3542aaec8fd7aaa7659911761fe3ccf5
zubinkhan <zubin.khan@mail.utoronto.ca>
2016-03-22 17:41:41 -0400

    update pages

--- page.php
+++ page.php
@@ -13,30 +13,44 @@
  */
 
 get_header(); ?>
+<?php
+	if ( is_front_page() && is_home()) {
+		// Include the featured content template.
+		get_template_part( 'featured-content' );
+	}
+?>
+</div>
+<?php
+?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		
-<div class="flexslider">
-  <ul class="slides">
-    <li>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/c/c4/Bildtankstelle_1_026.jpg" />
-    </li>
-    <li>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Bildtankstelle_1_011.jpg" />
-    </li>
-    <li>
-      <img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Bildtankstelle_1_059.jpg" />
-    </li>
-    <li>
-      <img src="https://commons.wikimedia.org/wiki/Category:Flower_bouquets#/media/File:Bildtankstelle_1_028.jpg" />
-    </li>
-  </ul>
-</div>
+			<?php
+		//    wp_reset_query(); //resetting the page query
+
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+					?>
+				<div class="entry-content-page">
+					<?php the_content(); ?> <!-- Page Content -->
+				</div><!-- .entry-content-page -->
+			<?php
+				endwhile;
+			?>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
 
 <?php
 get_sidebar();
