a6ffd1397f5d7f3827c8d77182a476fb46e29741
zubinkhan <zubin.khan@mail.utoronto.ca>
2016-03-21 21:04:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,13 +14,13 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
 
+	<div id="primary" class="content-area">
+		<div id="main" class="site-main" role="main">
 	<div id="gridcontainer">
 <?php 
 	$counter = 1; //start counter
-	$grids = 2; //Grids per row
+	$grids = 4; //Grids per row
 	$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 	$args = array('posts_per_page' => 5, 'paged' => $paged, 'cat'=>30 );
 	
@@ -29,7 +29,7 @@ get_header(); ?>
 ?>
 <?php
 //Show the left hand side column
-if($counter == 1) :
+if($counter < $grids) :
 ?>
 	<div class="griditemleft">
 	<div class="postimage">
@@ -54,17 +54,39 @@ endif;
 ?>
 <?php
 $counter++;
-endwhile; ?>
+endwhile; 
+if($counter > 1) 
+	echo '<div class="clear"></div>'
+?>
+
 <?php the_posts_pagination( $args ); ?>
-
-
 
 <?php endif;
 ?>
-</div>
 
-		</main><!-- #main -->
+		<?php
+			if ( have_posts() ) :
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+
+				endwhile;
+				// Previous/next post navigation.
+			else :
+				// If no content, include the "No posts found" template.
+				get_template_part( 'content', 'none' );
+			endif;
+		?>
+
+		</div><!-- #content -->
 	</div><!-- #primary -->
+	<?php get_sidebar( 'content' ); ?>
 <?php
 get_sidebar();
 get_footer();
